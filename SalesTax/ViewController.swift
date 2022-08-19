3096114181aaca02170e8c74944d0263b31d843a
Thomas Phillips <thomasphillips3@gmail.com>
2022-08-18 22:46:59 -0700

    Initial Commit

---
+++ SalesTax/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SalesTax
+//
+//  Created by Thomas Phillips on 8/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
