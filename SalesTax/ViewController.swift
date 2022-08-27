980c1797040ddd996dea3cc32664b2fc0fb6ea77
Thomas Phillips <thomasphillips3@gmail.com>
2022-08-26 21:46:08 -0700

    save entered values to a variable

--- SalesTax/ViewController.swift
+++ SalesTax/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var beforeTaxPrice: Float = 0.00
+    var salesTaxRate: Float = 0.00
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func beforeTaxPriceChanged(_ sender: UITextField) {
+        
+        if let value = Float(sender.text!) {
+            beforeTaxPrice = value
+        } else {
+            beforeTaxPrice = 0.00
+        }
+    }
+    
+    @IBAction func salesTaxRateChanged(_ sender: UITextField) {
+        if let value = Float(sender.text!) {
+            salesTaxRate = value
+        } else {
+            salesTaxRate = 0.00
+        }
+        
+    }
+    
 }
 
